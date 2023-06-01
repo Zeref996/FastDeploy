81a9c6944eb1e0605efe46f9ca59b7d58683cf1f
Zeref996 <825276847@qq.com>
2023-06-01 15:11:35 +0800

    add run_xpu.sh

---
+++ benchmark/cpp/run_xpu.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+bash benchmark_xpu.sh config/config.xpu.lite.fp32.e2e.mem.txt
+bash benchmark_xpu.sh config/config.xpu.lite.fp32.e2e.txt
+#bash benchmark_xpu.sh config/config.xpu.lite.fp32.l3.e2e.mem.txt
+#bash benchmark_xpu.sh config/config.xpu.lite.fp32.l3.e2e.txt
+#bash benchmark_xpu.sh config/config.xpu.lite.fp32.l3.txt
+#bash benchmark_xpu.sh config/config.xpu.lite.fp32.txt
